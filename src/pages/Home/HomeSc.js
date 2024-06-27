187335e17fb991c4aa723249dd61758dbda0a1e8
samcab28 <samircabrera2528@gmail.com>
2024-06-26 18:05:32 -0600

    update home

--- src/pages/Home/HomeSc.js
+++ src/pages/Home/HomeSc.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PrincipalMenu from '../../components/menu/PrincipalMenu';
-import Head from "../../components/Header/Head";
-import Foot from "../../components/Footer/Foot";
+import ResponsiveFooter from "../../components/responsiveFooter/ResponsiveFooter";
 
 const HomeSc = () => {
     return (
         <div>
-            <Head/>
-            <h1>Home Screen</h1>
-            <Foot />
+            <h1>Home Screen HOLAAAAAAAAAAAAAAAAAAAAAA PAMEEEEEEEEEEEEEEEEEEEEEEELA ejemplo union</h1>
+            <PrincipalMenu/>
+            <ResponsiveFooter/>
         </div>
     );
 };
