1f75761ff8609ea2b30df41aa8641cda3b04d51b
samcab28 <samircabrera2528@gmail.com>
2024-06-30 20:42:27 -0600

    funcionalidad guardar pdf rutina

--- src/pages/loggedAdmin/rutina/CrearRutinaSc.js
+++ src/pages/loggedAdmin/rutina/CrearRutinaSc.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { saveAs } from 'file-saver';
 import PrincipalMenu from '../../../components/menu/PrincipalMenu';
 import SelectSingleUsuarioByName from '../../../components/ui/selectSingle/selectSingleUsuarioByName';
 import DatePickerPrueba from '../../../components/datePicker/DatePickerPrueba';
 
 const CrearRutinaSc = () => {
+    const [step, setStep] = useState(1);
     const [formData, setFormData] = useState({
         usuario: "",
         fechaInicio: null,
@@ -11,9 +13,17 @@ const CrearRutinaSc = () => {
         cantSemana: 0
     });
 
+    const [funcionalidades, setFuncionalidades] = useState([]);
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+        setFormData((prevData) => {
+            const newFormData = { ...prevData, [name]: value };
+            if (name === 'cantSemana' && newFormData.fechaInicio) {
+                newFormData.fechaFin = calcularFechaFin(newFormData.fechaInicio, value);
+            }
+            return newFormData;
+        });
     };
 
     const handleUsuarioChange = (id) => {
@@ -21,10 +31,60 @@ const CrearRutinaSc = () => {
     };
 
     const handleDateChange = (name, date) => {
-        setFormData({ ...formData, [name]: date });
+        setFormData((prevData) => {
+            const newFormData = { ...prevData, [name]: date };
+            if (name === 'fechaInicio' && newFormData.cantSemana) {
+                newFormData.fechaFin = calcularFechaFin(date, newFormData.cantSemana);
+            }
+            return newFormData;
+        });
     };
 
-    const handleSubmit = async (e) => {
+    const calcularFechaFin = (fechaInicio, cantSemana) => {
+        const fechaInicioDate = new Date(fechaInicio);
+        const diasAdicionales = cantSemana * 7;
+        const fechaFinDate = new Date(fechaInicioDate);
+        fechaFinDate.setDate(fechaFinDate.getDate() + diasAdicionales);
+        return fechaFinDate;
+    };
+
+    const handleAddFuncionalidad = () => {
+        const nuevaFuncionalidad = {
+            nombreFuncionalidad: "",
+            ejercicios: []
+        };
+        setFuncionalidades([...funcionalidades, nuevaFuncionalidad]);
+    };
+
+    const handleAddEjercicio = (indexFuncionalidad) => {
+        const nuevoEjercicio = {
+            nombreEjercicio: "",
+            comentario: "",
+            ...Array.from({ length: formData.cantSemana }, (_, i) => ({
+                [`semana${i + 1}`]: ""
+            })).reduce((acc, curr) => ({ ...acc, ...curr }), {})
+        };
+
+        const nuevasFuncionalidades = [...funcionalidades];
+        nuevasFuncionalidades[indexFuncionalidad].ejercicios.push(nuevoEjercicio);
+        setFuncionalidades(nuevasFuncionalidades);
+    };
+
+    const handleFuncionalidadChange = (index, e) => {
+        const { name, value } = e.target;
+        const nuevasFuncionalidades = [...funcionalidades];
+        nuevasFuncionalidades[index][name] = value;
+        setFuncionalidades(nuevasFuncionalidades);
+    };
+
+    const handleEjercicioChange = (indexFuncionalidad, indexEjercicio, e) => {
+        const { name, value } = e.target;
+        const nuevasFuncionalidades = [...funcionalidades];
+        nuevasFuncionalidades[indexFuncionalidad].ejercicios[indexEjercicio][name] = value;
+        setFuncionalidades(nuevasFuncionalidades);
+    };
+
+    const handleSubmitInitial = (e) => {
         e.preventDefault();
         const { fechaInicio, fechaFin, cantSemana } = formData;
 
@@ -48,14 +108,23 @@ const CrearRutinaSc = () => {
             return;
         }
 
+        setStep(2);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
         const fechaInicioFinal = formData.fechaInicio.toISOString().split('T')[0];
         const fechaFinFinal = formData.fechaFin.toISOString().split('T')[0];
 
         const jsonData = {
             ...formData,
             fechaInicio: fechaInicioFinal,
-            fechaFin: fechaFinFinal
+            fechaFin: fechaFinFinal,
+            funcionalidades
         };
+
+        console.log(jsonData);
 
         // Envía los datos al backend para generar el PDF
         const response = await fetch('http://localhost:4000/servicioAPI/generarPDF', {
@@ -66,53 +135,110 @@ const CrearRutinaSc = () => {
             body: JSON.stringify(jsonData),
         });
 
-        // Crea un blob a partir de la respuesta
+        if (!response.ok) {
+            throw new Error('Error generando el PDF');
+        }
+
+        // Convierte la respuesta en un blob
         const blob = await response.blob();
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'prueba.pdf';
-        a.click();
-        window.URL.revokeObjectURL(url);
+
+        // Guarda el PDF en el navegador
+        saveAs(blob, `rutina_${formData.usuario}.pdf`); 
     };
 
     return (
         <div>
             <PrincipalMenu />
             <h1>Crear Rutina</h1>
-            <h2>Crear Rutina</h2>
 
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Seleccione el usuario al que pertenece la rutina
-                    <SelectSingleUsuarioByName onUsuarioChange={handleUsuarioChange} />
-                </label>
-                <label>
-                    Seleccione la fecha de inicio de la Rutina
-                    <DatePickerPrueba
-                        selected={formData.fechaInicio}
-                        onDateChange={(date) => handleDateChange('fechaInicio', date)}
-                    />
-                </label>
-                <label>
-                    Seleccione la fecha de finalizacion de la Rutina
-                    <DatePickerPrueba
-                        selected={formData.fechaFin}
-                        onDateChange={(date) => handleDateChange('fechaFin', date)}
-                    />
-                </label>
-                <label>
-                    Seleccione la cantidad de semanas de la Rutina
-                    <input
-                        type="number"
-                        name="cantSemana"
-                        value={formData.cantSemana}
-                        onChange={handleInputChange}
-                    />
-                </label>
-                <br />
-                <button type="submit">Generar Rutina</button>
-            </form>
+            {step === 1 && (
+                <form onSubmit={handleSubmitInitial}>
+                    <h2>Información Inicial</h2>
+                    <label>
+                        Seleccione el usuario al que pertenece la rutina
+                        <SelectSingleUsuarioByName onUsuarioChange={handleUsuarioChange} />
+                    </label>
+                    <label>
+                        Seleccione la fecha de inicio de la Rutina
+                        <DatePickerPrueba
+                            selected={formData.fechaInicio}
+                            onDateChange={(date) => handleDateChange('fechaInicio', date)}
+                        />
+                    </label>
+                    <br/>
+                    <label>
+                        Seleccione la cantidad de semanas de la Rutina
+                        <input
+                            type="number"
+                            name="cantSemana"
+                            value={formData.cantSemana}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+                    <br />
+                    <button type="submit">Guardar Información Inicial</button>
+                </form>
+            )}
+
+            {step === 2 && (
+                <form onSubmit={handleSubmit}>
+                    <h2>Agregar Funcionalidades y Ejercicios</h2>
+                    {funcionalidades.map((funcionalidad, indexFuncionalidad) => (
+                        <div key={indexFuncionalidad}>
+                            <label>
+                                Nombre de la funcionalidad
+                                <input
+                                    type="text"
+                                    name="nombreFuncionalidad"
+                                    value={funcionalidad.nombreFuncionalidad}
+                                    onChange={(e) => handleFuncionalidadChange(indexFuncionalidad, e)}
+                                />
+                            </label>
+                            <button type="button" onClick={() => handleAddEjercicio(indexFuncionalidad)}>
+                                Agregar Ejercicio
+                            </button>
+                            {funcionalidad.ejercicios.map((ejercicio, indexEjercicio) => (
+                                <div key={indexEjercicio}>
+                                    <label>
+                                        Nombre del ejercicio
+                                        <input
+                                            type="text"
+                                            name="nombreEjercicio"
+                                            value={ejercicio.nombreEjercicio}
+                                            onChange={(e) => handleEjercicioChange(indexFuncionalidad, indexEjercicio, e)}
+                                        />
+                                    </label>
+                                    <label>
+                                        Comentario
+                                        <input
+                                            type="text"
+                                            name="comentario"
+                                            value={ejercicio.comentario}
+                                            onChange={(e) => handleEjercicioChange(indexFuncionalidad, indexEjercicio, e)}
+                                        />
+                                    </label>
+                                    {Array.from({ length: formData.cantSemana }).map((_, semanaIndex) => (
+                                        <label key={semanaIndex}>
+                                            Semana {semanaIndex + 1}
+                                            <input
+                                                type="text"
+                                                name={`semana${semanaIndex + 1}`}
+                                                value={ejercicio[`semana${semanaIndex + 1}`] || ""}
+                                                onChange={(e) => handleEjercicioChange(indexFuncionalidad, indexEjercicio, e)}
+                                            />
+                                        </label>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                    <button type="button" onClick={handleAddFuncionalidad}>
+                        Agregar Funcionalidad
+                    </button>
+                    <br />
+                    <button type="submit">Generar Rutina</button>
+                </form>
+            )}
         </div>
     );
 };
