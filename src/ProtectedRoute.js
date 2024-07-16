dab4ee44ae5646c2baa8e61c481606788d54a30a
samcab28 <samircabrera2528@gmail.com>
2024-07-16 06:52:29 -0600

    implementacion seguridad

---
+++ src/ProtectedRoute.js
@@ -0,0 +1,12 @@
+// ProtectedRoute.js
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import UserContext from './UserContext';
+
+const ProtectedRoute = ({ element: Component, ...rest }) => {
+    const { user } = useContext(UserContext);
+
+    return user ? <Component {...rest} /> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
